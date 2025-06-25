3c43ae9ea07643d07edd4fbfe7259c8544038339
CJ1626 <149098341+CJ1626@users.noreply.github.com>
2025-06-26 00:07:44 +0530

    Update express.js

--- express.js
+++ express.js
@@ -1,46 +1,36 @@
-// server side
 const express = require("express");
-// express server
-const app = express();
-//  nodejs
-const server = require("http").Server(app);
-// nodejs => socket enabled
 const path = require("path");
-const io = require("socket.io")(server);
-// serve static assets to client
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Serve static files from the 'public' directory
 app.use(express.static("public"));
 
-// server
-io.on("connection", function(socket) {
-  socket.on("size", function(size) {
-    socket.broadcast.emit("onsize", size);
-  });
-  socket.on("color", function(color) {
-    socket.broadcast.emit("oncolor", color);
-  });
+// Socket.io connections
+io.on("connection", (socket) => {
+  console.log("New client connected:", socket.id);
 
-  socket.on("toolchange", function(tool) {
-    socket.broadcast.emit("ontoolchange", tool);
-  });
-  socket.on("hamburger", function() {
-    socket.broadcast.emit("onhamburger");
-  });
-  socket.on("mousedown", function(point) {
-    socket.broadcast.emit("onmousedown", 
-    point);
-  });
-  socket.on("mousemove", function(point) {
-    socket.broadcast.emit("onmousemove", point);
-  });
-  socket.on("undo", function() {
-    socket.broadcast.emit("onundo");
-  });
-  socket.on("redo", function() {
-    socket.broadcast.emit("onredo");
+  const forwardEvent = (eventName) => {
+    socket.on(eventName, (data) => {
+      socket.broadcast.emit("on" + eventName, data);
+    });
+  };
+
+  // List of custom events
+  const events = ["size", "color", "toolchange", "hamburger", "mousedown", "mousemove", "undo", "redo"];
+  events.forEach(forwardEvent);
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
   });
 });
-// nodejs server
-const port = process.env.PORT || 3000;
-server.listen(port, function(req, res) {
-  console.log("Server has started at port 3000");
+
+// Start server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
